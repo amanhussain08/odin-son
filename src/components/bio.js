2ac34d5d8ed8636a70f145791016844330dff27b
Aman Hussain <73658735+paaniik@users.noreply.github.com>
2021-03-03 16:49:19 +0530

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -45,7 +45,7 @@ const Bio = () => {
       {author?.name && (
         <p>
           Hello there, My name is <strong>{author.name}</strong> {author?.summary || null}
-          {` `} I am a First year Engineering student!
+          {` `} <br>
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
              Follow me on Twitter
           </a>
