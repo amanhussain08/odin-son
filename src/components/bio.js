42168fb241dc1ba95b6dc5ccac55046f4681cee8
Aman Hussain <73658735+paaniik@users.noreply.github.com>
2021-03-03 16:00:07 +0530

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,7 +44,7 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          Hello there, My name is <strong>{author.name}</strong> {author?.summary || null}
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
              Follow me on Twitter
